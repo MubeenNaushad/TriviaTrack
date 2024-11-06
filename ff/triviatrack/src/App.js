c401ccb74870b4362d00defcebd0b13fabe1ca26
Mubeen <114735488+MubeenNaushad@users.noreply.github.com>
2024-11-06 17:40:49 +0500

    homepage

---
+++ ff/triviatrack/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from './LandingPage';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function App() {
+    return (
+        <div className="App">
+            <LandingPage />
+        </div>
+    );
+}
+
+export default App;
