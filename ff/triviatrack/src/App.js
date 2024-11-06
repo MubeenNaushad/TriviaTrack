97735ca6e1061bc7d30309754156e616f30d9be1
Mubeen <114735488+MubeenNaushad@users.noreply.github.com>
2024-11-06 22:04:51 +0500

    Dashboard

--- ff/triviatrack/src/App.js
+++ ff/triviatrack/src/App.js
@@ -3,6 +3,10 @@ import LandingPage from './LandingPage';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Login from './Login';
 import Signup from './Signup';
+import Dashboard from './DashboardPage';
+import TeacherDashboard from './TeacherDashboardPage';
+import ContactForm from './ContactForm';
+
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
@@ -13,6 +17,9 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/teacherdashboard" element={<TeacherDashboard />} />
+          <Route path="/contact" element={<ContactForm />} />
         </Routes>
       </div>
     </Router>
