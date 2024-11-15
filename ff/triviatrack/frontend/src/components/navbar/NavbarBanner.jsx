982bc0cea806e056aa005cebe9b2cd1b5344e8a4
Mubeen <114735488+MubeenNaushad@users.noreply.github.com>
2024-11-16 03:20:42 +0500

    fix

--- ff/triviatrack/frontend/src/components/navbar/NavbarBanner.jsx
+++ ff/triviatrack/frontend/src/components/navbar/NavbarBanner.jsx
@@ -9,7 +9,7 @@ const NavbarBanner = () => {
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             transition={{ duration: 0.5, delay: 0.9 }}
-            className='bg-primary text-sm text-center font-semibold p-1 hidden lg:block relative top-[5.5rem] sticky z-40'>
+            className='bg-primary text-sm text-center font-semibold p-1 hidden lg:block top-[5.5rem] sticky z-40'>
                 Are you a trivia master? Test your knowledge with TriviaTrack!
             <a href="#" className='text-seconday ml-2'>Talk to us</a>
             <div className='absolute top-1/2 right-10 cursor-pointer -translate-y-1/2' onClick={() => setIsOpen(false)}>
