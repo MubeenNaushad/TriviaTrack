f92f39c593ea0c71e7d44de77e69161b7cd7fc3c
Mubeen <114735488+MubeenNaushad@users.noreply.github.com>
2025-01-19 00:50:53 +0500

    Verficiation & Reset

--- ff/triviatrack/backend/index.js
+++ ff/triviatrack/backend/index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDB " + process.env.MONGO_URI);
     })
     .catch((err) => {
-        console.log(err);
+        console.log(err); 
     });
 
 // Routes
