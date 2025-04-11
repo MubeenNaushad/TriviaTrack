e768ef9f9f9771edbb12afc808a315a856f98564
Mubeen <114735488+MubeenNaushad@users.noreply.github.com>
2025-04-11 17:45:33 +0500

    Changes

---
+++ ff/triviatrack/backend/models/quiz.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+const OptionSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  isCorrect: { type: Boolean, default: false },
+});
+
+
+const QuestionSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  type: { type: String, enum: ["multiple-choice", "true-false"], required: true },
+  options: [OptionSchema],
+});
+
+
+const QuizSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    questions: [QuestionSchema],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+export const Quiz = mongoose.model("Quiz", QuizSchema);
